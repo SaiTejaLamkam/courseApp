9d5da654f2383cb31294adc33e8b2ff49e0483e8
Sai Personal <saiteja776@gmail.com>
2018-05-21 02:47:35 +0530

    Changes

--- src/shared/directives/header/header.js
+++ src/shared/directives/header/header.js
@@ -3,7 +3,8 @@
 	module.exports = [function () {
 		var headerController;
 		headerController = function ($scope, $rootScope, $state) {
-            var vm = this;
+			var vm = this;
+			
             $(".dropdown").hover(            
                 function() {
                     $('.multi-level', this).stop( true, true ).fadeIn("fast");
@@ -14,7 +15,47 @@
                     $('.multi-level', this).stop( true, true ).fadeOut("fast");
                     $(this).toggleClass('open');
                     // $('b', this).toggleClass("caret caret-up");                
-                });
+			    });
+			
+
+
+
+
+
+
+			$(document).ready(function() {
+
+				var toggleAffix = function(affixElement, scrollElement, wrapper) {
+				
+				  var height = affixElement.outerHeight(),
+					  top = wrapper.offset().top;
+				  
+				  if (scrollElement.scrollTop() >= top){
+					  wrapper.height(height);
+					  affixElement.addClass("affix");
+				  }
+				  else {
+					  affixElement.removeClass("affix");
+					  wrapper.height('auto');
+				  }
+					
+				};
+				
+			  
+				$('[data-toggle="affix"]').each(function() {
+				  var ele = $(this),
+					  wrapper = $('<div></div>');
+				  
+				  ele.before(wrapper);
+				  $(window).on('scroll resize', function() {
+					  toggleAffix(ele, $(this), wrapper);
+				  });
+				  
+				  // init
+				  toggleAffix(ele, $(window), wrapper);
+				});
+				
+			  });
 			
 		};
 		headerController.$inject = ['$scope', '$rootScope', '$state'];
