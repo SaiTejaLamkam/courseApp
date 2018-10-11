ce1cc072bc80c1e570f4b46bbd72c32ad96a275c
Lamkam <sai.teja.lamkam@spglobal.com>
2018-10-11 16:09:25 +0530

    changes

--- src/modules/modules.js
+++ src/modules/modules.js
@@ -2,10 +2,10 @@
 	'use strict';
 	require('./home/home.module');
 	require('./sasTrainingDetails/sasTrainingDetails.module');
-	require('./aboutUs/aboutUs.module');
+	// require('./aboutUs/aboutUs.module');
 	module.exports = angular.module('prohires.modules', [
 		'prohires.module.home',
 		'prohires.module.sasTrainingDetails',
-		'prohires.module.aboutUs'
+		// 'prohires.module.aboutUs'
 	]);
 })();
